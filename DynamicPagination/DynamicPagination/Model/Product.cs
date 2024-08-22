b4e2b80edb9bcf0b65b1b1ea76fe8b6da6da8658
Rijwan Ansari <rijwan.7475@gmail.com>
2024-08-22 21:15:04 +0800

    Add project files.

---
+++ DynamicPagination/DynamicPagination/Model/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace DynamicPagination.Model;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
+
+//sample dummy data
+public class ProductService
+{
+    public List<Product> GetProducts()
+    {
+        // Sample data
+        return Enumerable.Range(1, 100).Select(i => new Product
+        {
+            Id = i,
+            Name = $"Product {i}",
+            Price = i * 10
+        }).ToList();
+    }
+}
